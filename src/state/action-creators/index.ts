1fb0405b854485dc0ad53dad1f02461426d3ce52
yuenu <yuenuni@hotmail.com>
2021-10-07 22:47:04 +0800

    feat: writing with typescript

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,32 @@
+import { ActionType } from '../action-types'
+import { Dispatch } from 'redux'
+import type { Action} from '../actions'
+
+
+export const depositMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.DEPOSIT,
+      payload: amount
+    })
+  }
+}
+
+
+export const withdrawMoney = (amount: number) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.WITHDRAW,
+      payload: amount
+    })
+  }
+}
+
+
+export const bankrupt = () => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BANKRUPT
+    })
+  }
+}
---
+++ src/state/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import bankReducer from "./bankReducer";
+
+const reducer = combineReducers({
+  bank: bankReducer,
+});
+
+export default reducer;
+
+export type State = ReturnType<typeof reducer>
